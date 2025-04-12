0600f4e0f06d0a083e95ceb6df3812d7b422b9bc
Asya I <as.izmaylova@gmail.com>
2025-04-12 19:14:15 +0300

    applied prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,56 @@
-import ReactDOM from 'react-dom'
-import { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
-import TaskForm from './TaskForm';
-import ProjectForm from './ProjectForm';
+import ReactDOM from "react-dom";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import TaskForm from "./TaskForm";
+import ProjectForm from "./ProjectForm";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [showProjectForm, setShowProjectForm] = useState(false)
-  const [projectName, setProjectName] = useState('');
-  const [projects, setProjects] = useState([{ id: uuidv4(), name: "first project" }]);
+  const [showProjectForm, setShowProjectForm] = useState(false);
+  const [projectName, setProjectName] = useState("");
+  const [projects, setProjects] = useState([
+    { id: uuidv4(), name: "first project" },
+  ]);
   const [selectedProjectId, setSelectedProjectId] = useState(null);
 
-  const [showTaskForm, setShowTaskForm] = useState(false)
-  const [tasks, setTasks] = useState([]);  
+  const [showTaskForm, setShowTaskForm] = useState(false);
+  const [tasks, setTasks] = useState([]);
   const [taskData, setTaskData] = useState({
-    title: '',
-    description: '',
-    date: '',
-    priority: 'none',
+    title: "",
+    description: "",
+    date: "",
+    priority: "none",
     projectId: null,
   });
-  
 
   function handleProjectForm() {
-    setShowProjectForm(!showProjectForm)
+    setShowProjectForm(!showProjectForm);
   }
 
   function handleTaskForm() {
     if (projects.length === 0) {
       alert("Please create project");
       return;
-    } 
+    }
     if (!selectedProjectId) {
       alert("Please select project");
       return;
     }
-    setShowTaskForm(!showTaskForm)
+    setShowTaskForm(!showTaskForm);
   }
 
   function handleAddClick() {
-    if(projectName.trim()) {
+    if (projectName.trim()) {
       const newProject = {
         id: uuidv4(),
         name: projectName,
       };
-      setProjects([...projects, newProject])
-      setProjectName('');
-      setShowProjectForm(false)
+      setProjects([...projects, newProject]);
+      setProjectName("");
+      setShowProjectForm(false);
     }
   }
 
@@ -58,23 +59,23 @@ function App() {
       const newTask = {
         ...taskData,
         id: uuidv4(),
-        projectId: selectedProjectId,  // чтобы привязать задачи к конкретному проекту (присваиваем id проекта)
+        projectId: selectedProjectId, // чтобы привязать задачи к конкретному проекту (присваиваем id проекта)
       };
-      setTasks((task) => [...task, newTask])
+      setTasks((task) => [...task, newTask]);
       setTaskData({
-        title: '',
-        description: '',
-        date: '',
-        priority: 'none',
+        title: "",
+        description: "",
+        date: "",
+        priority: "none",
         projectId: null,
-      })
+      });
       setShowTaskForm(false);
     }
   }
 
   function removeProject(id) {
-    const updatedProjectList = projects.filter(project => project.id !== id)
-    const updatedTaskList = tasks.filter(task => task.projectId !== id); // удаляем задачи удаленного проекта
+    const updatedProjectList = projects.filter((project) => project.id !== id);
+    const updatedTaskList = tasks.filter((task) => task.projectId !== id); // удаляем задачи удаленного проекта
     setProjects(updatedProjectList);
     setTasks(updatedTaskList);
     if (selectedProjectId == id) {
@@ -83,7 +84,7 @@ function App() {
   }
 
   function removeTask(id) {
-    const updatedTaskList = tasks.filter(task => task.id !== id)
+    const updatedTaskList = tasks.filter((task) => task.id !== id);
     setTasks(updatedTaskList);
   }
 
@@ -92,65 +93,102 @@ function App() {
       <div className="project-container">
         <h2>Projects</h2>
         <div>
-          <button className="add-button" onClick={handleProjectForm}>+</button>
-          <p style={{ fontSize: "1.1rem"}}>Add project</p>
+          <button className="add-button" onClick={handleProjectForm}>
+            +
+          </button>
+          <p style={{ fontSize: "1.1rem" }}>Add project</p>
         </div>
 
         {showProjectForm && (
-          <ProjectForm projectName={projectName} setProjectName={setProjectName} 
-          handleAddClick={handleAddClick} handleProjectForm={handleProjectForm} />
+          <ProjectForm
+            projectName={projectName}
+            setProjectName={setProjectName}
+            handleAddClick={handleAddClick}
+            handleProjectForm={handleProjectForm}
+          />
         )}
- 
+
         {projects.length > 0 && (
-            <ul>
-              {projects.map((project) => (
-                  <li key={project.id}
-                  onClick={() => setSelectedProjectId(project.id)}
-                  style={{ 
-                    fontWeight: project.id === selectedProjectId ? "700" : "400",
-                    color: project.id === selectedProjectId ? "rgb(227, 121, 181)" : "grey" }}>
-                    {project.name}
-                  <button className="trash-btn" style={{ marginLeft: '20px' }} onClick={() => removeProject(project.id)}>    
-                  <FontAwesomeIcon icon={faTrash} /></button>
-                  </li>
-                ))}
-            </ul>
-          )
-        }       
+          <ul>
+            {projects.map((project) => (
+              <li
+                key={project.id}
+                onClick={() => setSelectedProjectId(project.id)}
+                style={{
+                  fontWeight: project.id === selectedProjectId ? "700" : "400",
+                  color:
+                    project.id === selectedProjectId
+                      ? "rgb(227, 121, 181)"
+                      : "grey",
+                }}
+              >
+                {project.name}
+                <button
+                  className="trash-btn"
+                  style={{ marginLeft: "20px" }}
+                  onClick={() => removeProject(project.id)}
+                >
+                  <FontAwesomeIcon icon={faTrash} />
+                </button>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
 
       <div className="task-container">
         <h2>Tasks</h2>
-        <div className='task-container__block'>
-          <button className="add-button" onClick={handleTaskForm}>+</button>
-          <p style={{ fontSize: "1.1rem"}}>Add task</p>
+        <div className="task-container__block">
+          <button className="add-button" onClick={handleTaskForm}>
+            +
+          </button>
+          <p style={{ fontSize: "1.1rem" }}>Add task</p>
         </div>
 
-      {showTaskForm && selectedProjectId && (
-        <TaskForm taskData={taskData} setTaskData={setTaskData} 
-        handleAddTask={handleAddTask} handleTaskForm={handleTaskForm}/>
-      )}
+        {showTaskForm && selectedProjectId && (
+          <TaskForm
+            taskData={taskData}
+            setTaskData={setTaskData}
+            handleAddTask={handleAddTask}
+            handleTaskForm={handleTaskForm}
+          />
+        )}
 
         {tasks
           .filter((task) => task.projectId === selectedProjectId)
           .map((task) => (
             <div className="task-card" key={task.id}>
-              <p style={{marginBottom: "10px", color: "white"}}><strong>{task.title}</strong></p>
-              <div style={{ display: "flex"}}>
-                <p className='task-card__description'>{task.description}</p>                
-                <div style={{ marginLeft: "30px", display: "flex", flexDirection: "column", color: "white" }}>
+              <p style={{ marginBottom: "10px", color: "white" }}>
+                <strong>{task.title}</strong>
+              </p>
+              <div style={{ display: "flex" }}>
+                <p className="task-card__description">{task.description}</p>
+                <div
+                  style={{
+                    marginLeft: "30px",
+                    display: "flex",
+                    flexDirection: "column",
+                    color: "white",
+                  }}
+                >
                   <p>{task.date}</p>
-                  <p className='task-card__priority'>{task.priority}</p>
+                  <p className="task-card__priority">{task.priority}</p>
                 </div>
-                <button className="trash-btn" onClick={() => removeTask(task.id)}>
-                <FontAwesomeIcon icon={faTrash} style={{color: "#ffffff",}} /></button>   
+                <button
+                  className="trash-btn"
+                  onClick={() => removeTask(task.id)}
+                >
+                  <FontAwesomeIcon
+                    icon={faTrash}
+                    style={{ color: "#ffffff" }}
+                  />
+                </button>
               </div>
-           </div>
-          ))
-        }   
+            </div>
+          ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/ProjectForm.jsx
+++ src/ProjectForm.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
+import React from "react";
 
-function ProjectForm({ projectName, setProjectName, handleAddClick, handleProjectForm}) {
+function ProjectForm({
+  projectName,
+  setProjectName,
+  handleAddClick,
+  handleProjectForm,
+}) {
   return (
     <div className="project-form">
-    <h3>Enter project name</h3>
-    <input
-      type="text" 
-      value={projectName}
-      onChange={(e) => setProjectName(e.target.value)}/>
-    <div className='button-container'>
-      <button type="button" onClick={handleAddClick}>Add</button>
-      <button type="button" onClick={handleProjectForm}>Cancel</button>          
+      <h3>Enter project name</h3>
+      <input
+        type="text"
+        value={projectName}
+        onChange={(e) => setProjectName(e.target.value)}
+      />
+      <div className="button-container">
+        <button type="button" onClick={handleAddClick}>
+          Add
+        </button>
+        <button type="button" onClick={handleProjectForm}>
+          Cancel
+        </button>
+      </div>
     </div>
-  </div>
-  )
+  );
 }
 
-export default ProjectForm
+export default ProjectForm;
